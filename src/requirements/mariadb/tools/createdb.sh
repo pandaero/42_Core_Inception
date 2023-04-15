4b9249edde2bfb8ec1f60dcad316652fb711e61d
pandalaf <pandalaf@student.42wolfsburg.de>
2023-04-15 18:45:32 +0200

    hopefully correct config for mariadb container

---
+++ src/requirements/mariadb/tools/createdb.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+# CHANGE THESE VALUES
+ROOT_PASS=ROOTPASS
+DB_NAME=NAME
+DB_USER=USER
+DB_PASS=PASS
+
+# Check mySQL running, start it if not.
+if [ ! -d "/varl/lib/mysql/mysql" ]; then
+	chown -R mysql:mysql /var/lib/mysql
+	mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql
+	tempfile=`mktemp`
+	if [ ! -f "$tempfile" ]; then
+		return 1
+	fi
+fi
+
+# Make Wordpress database script
+if [ ! -d "/var/lib/mysql/wordpress" ]; then
+	# Prepare SQL script to create Wordpress database
+	DB_FILE=$( dirname -- "$0" )/createdb.sql
+	cat << EOF > /$DB_FILE
+USE mysql;
+FLUSH PRIVILEGES;
+DELETE FROM mysql.user WHERE User='';
+DROP DATABASE test;
+DELETE FROM mysql.db WHERE Db='test';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$ROOT_PASS';
+CREATE DATABASE $DB_NAME CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';
+GRANT ALL PRIVILIEGES ON wordpress.* TO '$DB_USER'@'%';
+FLUSH PRIVILEGES;
+EOF
+fi
